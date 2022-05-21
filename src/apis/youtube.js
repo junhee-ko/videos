35b3dc16912ad798bde3ccc75c59e064b78bee74
junhee-ko <junheee.ko@gmail.com>
2022-05-21 17:29:15 +0900

    Hide youtube API Key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const KEY = 'AIzaSyBUuGtnr4rXq-aR1TV1HdttnjD0WqQ6mIs'
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
