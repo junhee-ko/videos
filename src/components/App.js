b205892ce435148806606d14c4df5e50109a7b41
junhee-ko <junheee.ko@gmail.com>
2022-05-21 15:44:33 +0900

    Add videos state in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,19 @@ import youtube from "../apis/youtube";
 
 class App extends React.Component {
 
-  onTermSubmit = (term) => {
-    youtube.get('/search', {
+  state = {
+    videos: []
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
+    })
+
+    this.setState({
+      videos: response.data.items
     })
   }
 
@@ -16,6 +24,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit}/>
+        I have {this.state.videos.length} videos.
       </div>
     );
   }
