5bc838068bfe4590b424beeac63bc7c0065d2d37
junhee-ko <junheee.ko@gmail.com>
2022-05-21 15:32:13 +0900

    Add axios and api client

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyBUuGtnr4rXq-aR1TV1HdttnjD0WqQ6mIs'
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+})
+
